25e05fa25ae7028bcf6a79168c2c0e96e348e67b
Raymond Preston <rprest2@hyper-01-prod-ondemand-241-199.ec2.internal>
2026-04-16 23:23:27 +0000

    updated consensus_peaks.R to only export the consensus peaks if not previously created. Additionally, saves heatmaps to output/

--- Scripts/Generate_Consensus_Peaks.R
+++ Scripts/Generate_Consensus_Peaks.R
@@ -21,8 +21,13 @@ KPC_all = dba(sampleSheet=samples_all)
 KPC_all_counts = dba.count(KPC_all, summits = 250)
 
 # Extract the full consensus peak set (all peaks, not just DA)
-consensus_peaks <- dba.peakset(KPC_all_counts, bRetrieve=TRUE)
-export(consensus_peaks, "consensus_peaks_all.bed")
+if (!file.exists("input/consensus_peaks_all.bed")) {
+    print("Generating consensus_peaks_all.bed...")
+    consensus_peaks <- dba.peakset(KPC_all_counts, bRetrieve=TRUE)
+    export(consensus_peaks, "input/consensus_peaks_all.bed")
+} else {
+    print("consensus_peaks_all.bed already exists in input/, skipping export.")
+}
 
 #normalize by total reads in peaks
 KPC_all_counts_norm_trip = dba.normalize(KPC_all_counts, library=DBA_LIBSIZE_PEAKREADS)
@@ -40,15 +45,17 @@ met_vs_nonmet_trip.DB_down = met_vs_nonmet_trip.DB[met_vs_nonmet_trip.DB$Fold<0,
 met_vs_nonmet_trip.DB_complete <- dba.report(KPC_all_counts_norm_trip_DA, th=1, contrast=2)
 
 #annotate peaks
+dir.create("output", showWarnings = FALSE)
+
 anno = annotatePeak(met_vs_nonmet_trip.DB,  TxDb = TxDb.Mmusculus.UCSC.mm10.knownGene, annoDb="org.Mm.eg.db")
 anno_df = as.data.frame(anno)
 anno_df = anno_df[order(anno_df$Fold),]
-saveRDS(anno_df, file = "annotated_differentially_accessible_peaks_no_H10.RDS")
+saveRDS(anno_df, file = "output/annotated_differentially_accessible_peaks_no_H10.RDS")
 
 anno_complete = annotatePeak(met_vs_nonmet_trip.DB_complete,  TxDb = TxDb.Mmusculus.UCSC.mm10.knownGene, annoDb="org.Mm.eg.db")
 anno_complete_df <- as.data.frame(anno_complete)
 anno_complete_df = anno_complete_df[order(-anno_complete_df$Fold),]
-saveRDS(anno_complete_df, file = "annotated_complete_peakset_no_H10.RDS")
+saveRDS(anno_complete_df, file = "output/annotated_complete_peakset_no_H10.RDS")
 
 #generate heatmap
 report_df = as.data.frame(met_vs_nonmet_trip.DB)
@@ -61,5 +68,8 @@ differential_peak_counts <- differential_peak_counts[,-c(1,2,3)]
 mat = data.matrix(differential_peak_counts)
 log2mat = log2(1+mat)
 log2mat = t(scale(t(log2mat)))
+
+pdf("output/KPC_Consensus_Heatmap.pdf")
 Heatmap(log2mat, show_row_names = FALSE, cluster_rows = FALSE)
+dev.off()
            
